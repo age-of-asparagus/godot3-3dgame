b8db4096e996fa30146c5790ef89c77f33459ccc
Age of Asparagus <tylerecouture@gmail.com>
2021-03-07 19:13:19 -0800

    Episode 6 complete

--- 3DGame/Enemy.gd
+++ 3DGame/Enemy.gd
@@ -5,31 +5,95 @@ class_name Enemy
 onready var nav = $"../Navigation" as Navigation
 onready var player = $"../Player" as KinematicBody
 
+onready var attack_timer = $AttackTimer
+
+var default_material = load("res://Enemy/EnemyDefaultMaterial.tres")
+var attack_material = load("res://Enemy/EnemyAttackMaterial.tres")
+var resting_material = load("res://Enemy/EnemyRestingMaterial.tres")
+
 var path = []
 var current_node = 0
 var speed = 2
+var attack_speed_multiplier = 5
+
+var attack_target: Vector3
+var return_target: Vector3
+
+enum state {
+	SEEKING,
+	ATTACKING,
+	RETURNING,
+	RESTING,
+}
+
+var current_state = state.SEEKING
+
 
 func _ready():
-	pass  
+	$MeshInstance.set_surface_material(0, default_material)
 
 func _physics_process(delta):
-		
-	if current_node < path.size():
-		var direction: Vector3 = path[current_node] - global_transform.origin
-		if direction.length() < 1:
-			current_node += 1
-		else:
-			move_and_slide(direction.normalized() * speed)
-
+	
+	match current_state:
+		state.SEEKING:	
+			if current_node < path.size():
+				var direction: Vector3 = path[current_node] - global_transform.origin
+				if direction.length() < 1:
+					current_node += 1
+				else:
+					move_and_slide(direction.normalized() * speed)
+		state.ATTACKING:
+			move_and_attack()
+		state.RETURNING:
+			move_and_attack()
+		state.RESTING:
+			print("Resting!")			
+			
+func move_and_attack():
+	var attack_vector: Vector3 = attack_target - global_transform.origin
+	var direction: Vector3 = attack_vector.normalized()
+	var distance = attack_vector.length()
+	
+	print("I'm this far away from my target: ", distance)
+	move_and_slide(direction * speed * attack_speed_multiplier)
+	
+	if distance < 1:
+		match current_state:
+			state.ATTACKING:
+				current_state = state.RETURNING
+				attack_target = return_target
+			state.RETURNING:
+				current_state = state.RESTING
+				$MeshInstance.set_surface_material(0, resting_material)
+				collide_with_other_enemies(true)
+				attack_timer.start()
+				
+func collide_with_other_enemies(should_we_collide):
+	set_collision_mask_bit(2, should_we_collide)
+	set_collision_layer_bit(2, should_we_collide)
+	
 func update_path(target_position):
 	path = nav.get_simple_path(global_transform.origin, target_position)
 	
 
-func _on_Timer_timeout():
+func _on_Stats_you_died_signal():
+	queue_free()
+
+func _on_AttackRadius_body_entered(body):
+	if body == player:
+		attack_target = player.global_transform.origin
+		return_target = global_transform.origin
+		current_state = state.ATTACKING
+		$MeshInstance.set_surface_material(0, attack_material)
+		collide_with_other_enemies(false)
+
+
+func _on_PathUpdateTimer_timeout():
 #	print("Looking for Player!")
 	update_path(player.global_transform.origin)
 	current_node = 0
 
 
-func _on_Stats_you_died_signal():
-	queue_free()
+func _on_AttackTimer_timeout():
+	current_state = state.SEEKING
+	$MeshInstance.set_surface_material(0, default_material)
