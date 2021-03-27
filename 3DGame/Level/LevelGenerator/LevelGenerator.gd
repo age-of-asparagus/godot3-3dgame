98b62a7f101b92179dcdcfc7c500a1d446558d10
Age of Asparagus <tylerecouture@gmail.com>
2021-03-27 09:40:22 -0700

    2.2.1 Better random and seeding

--- 3DGame/Level/LevelGenerator/LevelGenerator.gd
+++ 3DGame/Level/LevelGenerator/LevelGenerator.gd
@@ -7,9 +7,29 @@ export var ObstacleScene: PackedScene
 export(int, 1, 21) var map_width = 11 setget set_width
 export(int, 1, 15) var map_depth = 11 setget set_depth
 
-export(float, 0, 1, 0.05) var obstacle_ratio = 0.2 setget set_obstacle_ratio
+export(float, 0, 1, 0.05) var obstacle_density = 0.2 setget set_obstacle_density
+
+export(int) var rng_seed = 12345 setget set_seed
+
+var map_coords_array := []
+
+class Coord:
+	var x: int
+	var z: int
+	
+	func _init(x, z):
+		self.x = x
+		self.z = z
+		
+	func _to_string():
+		# (x, z)
+		return "(" + str(x) + ", " + str(z) + ")"
 
 func _ready():
+	generate_map()
+	
+func set_seed(var new_val):
+	rng_seed = new_val
 	generate_map()
 	
 func set_width(var new_val):
@@ -20,8 +40,8 @@ func set_depth(var new_val):
 	map_depth = make_odd(new_val, map_depth)
 	generate_map()
 	
-func set_obstacle_ratio(new_val):
-	obstacle_ratio = new_val
+func set_obstacle_density(new_val):
+	obstacle_density = new_val
 	generate_map()
 	
 func make_odd(new_int, old_int):
@@ -32,6 +52,12 @@ func make_odd(new_int, old_int):
 			return new_int - 1
 	else: # it's already odd
 		return new_int
+		
+func fill_map_coords_array():
+	map_coords_array = []
+	for x in range(map_width):
+		for z in range(map_depth):
+			map_coords_array.append(Coord.new(x, z))
 	
 func generate_map():
 	print("Bleep bloop generating map...")
@@ -51,11 +77,22 @@ func add_ground():
 	add_child(ground)
 	
 func add_obstacles():
-	for x in range (map_width):
-		for z in range(map_depth):
-			if randf() < obstacle_ratio:  # create an obstacle at this place
-				var obstacle_position = Vector3(x * 2, 1, z * 2)
-				obstacle_position += Vector3(-map_width + 1, 0, -map_depth + 1)
-				var new_obstacle: CSGBox = ObstacleScene.instance()
-				new_obstacle.transform.origin = obstacle_position
-				add_child(new_obstacle)
+	fill_map_coords_array()
+#	print(map_coords_array)
+	seed(rng_seed)
+	map_coords_array.shuffle()
+#	print(map_coords_array)
+	
+	var num_obstacles: int = map_coords_array.size() * obstacle_density
+	
+	if num_obstacles > 0:
+		for coord in map_coords_array.slice(0, num_obstacles - 1):
+			create_obstacle_at(coord.x, coord.z)
+
+
+func create_obstacle_at(x, z):
+	var obstacle_position = Vector3(x * 2, 1, z * 2)
+	obstacle_position += Vector3(-map_width + 1, 0, -map_depth + 1)
+	var new_obstacle: CSGBox = ObstacleScene.instance()
+	new_obstacle.transform.origin = obstacle_position
+	add_child(new_obstacle)
