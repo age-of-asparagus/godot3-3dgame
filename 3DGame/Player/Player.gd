ef23c5fa5ecb8845dc67da41df35ab7295c101b7
Age of Asparagus <tylerecouture@gmail.com>
2021-06-04 20:42:06 -0700

    Falling into the void!

--- 3DGame/Player/Player.gd
+++ 3DGame/Player/Player.gd
@@ -2,27 +2,38 @@ extends KinematicBody
 
 onready var gun_controller = $GunController
 
-var speed = 8
-var velocity = Vector3()
+var speed = 0
+var max_speed = 8
+var velocity := Vector3()
+var move_direction := Vector3()
+const GRAVITY = 5
+const ACCEL = 1
+var falling_speed = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	
 	# Movement
-	velocity = Vector3()
+	move_direction = Vector3()
 	
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
+		move_direction.x += 1
 	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
+		move_direction.x -= 1
 	if Input.is_action_pressed("ui_up"):
-		velocity.z -= 1
+		move_direction.z -= 1
 	if Input.is_action_pressed("ui_down"):
-		velocity.z += 1
+		move_direction.z += 1
 		
-	velocity = velocity.normalized() * speed
-		
-	move_and_slide(velocity)
+	if not move_direction:
+		velocity = velocity.move_toward(Vector3(), ACCEL)
+	else:
+		velocity = velocity.move_toward(move_direction.normalized() * max_speed, ACCEL)
+	
+	falling_speed -= GRAVITY
+	var result = move_and_slide(Vector3(velocity.x, falling_speed, velocity.z))
+	falling_speed = result.y
+	
 	
 	# Shoot shoot
 	if Input.is_action_pressed("primary_action"):
@@ -31,4 +42,8 @@ func _physics_process(delta):
 
 func _on_Stats_you_died_signal():
 	print("GAME OVER")
+	queue_free()
+
+func _on_Void_body_entered(body):
+	print("AHHHHHHHHH!!!!!!!")
 	queue_free()
